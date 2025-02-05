a222b745d779a3785a908e061c6286dca094f117
pubert-Pebble <robwaldronplayz@gmail.com>
2025-02-05 15:32:19 -0500

    RELEASE USERSCRIPT

--- mova.user.js
+++ mova.user.js
@@ -1,1 +1,23 @@
--- soon
+// ==UserScript==
+// @name         Mova Client
+// @namespace    https://discod.gg/RWc3wB3fDt
+// @icon         https://login.i-ready.com/favicon.ico
+// @version      v1
+// @description  An iReady Cheat
+// @author       Mova Team
+// @match        https://login.i-ready.com/*
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @updateURL    https://github.com/pubert-Pebble/mova/raw/refs/heads/main/mova.user.js
+// @downloadURL  https://github.com/pubert-Pebble/mova/raw/refs/heads/main/mova.user.js
+// @require      https://pastebin.com/raw/uYmZnnUH
+// ==/UserScript==
+
+/*
+ __    __     ______     __   __   ______
+/\ "-./  \   /\  __ \   /\ \ / /  /\  __ \
+\ \ \-./\ \  \ \ \/\ \  \ \ \'/   \ \  __ \
+ \ \_\ \ \_\  \ \_____\  \ \__|    \ \_\ \_\
+  \/_/  \/_/   \/_____/   \/_/      \/_/\/_/
+
+*/
